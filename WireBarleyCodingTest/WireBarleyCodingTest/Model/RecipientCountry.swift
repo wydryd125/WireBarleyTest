f519fbaf8bb215ea63b5fa23f12a0376774c8b1f
wydryd125 <ljj4904@gmail.com>
2020-06-08 21:12:36 +0900

    Add: Folder_Model

---
+++ WireBarleyCodingTest/WireBarleyCodingTest/Model/RecipientCountry.swift
@@ -0,0 +1,12 @@
+//
+//  RecipientCountry.swift
+//  WireBarleyCodingTest
+//
+//  Created by 정유경 on 2020/06/08.
+//  Copyright © 2020 yukyung. All rights reserved.
+//
+
+import Foundation
+
+private enum Recipient {
+}
